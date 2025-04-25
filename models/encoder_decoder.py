d8100eb0fde750d7827b913745e7f910c6f9b464
Ahad Hamirani <ahamirani3@gatech.edu>
2025-04-25 16:10:39 +0530

    encoder_decoder_model
    Data consumption and encoder decoder model

---
+++ models/encoder_decoder.py
@@ -0,0 +1,56 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class EncoderDecoder(nn.Module):
+    def __init__(self, vocab_size, device, hidden_dim=128, num_heads=2,
+                 dim_feedforward=2048, num_layers_enc=2, num_layers_dec=2, dropout=0.2, max_length=43, p_tags=5,ignore_index = 1):
+        super(EncoderDecoder, self).__init__()
+
+        self.num_heads = num_heads
+        self.word_embedding_dim = hidden_dim
+        self.hidden_dim = hidden_dim
+        self.dim_feedforward = dim_feedforward
+        self.max_length = max_length
+        self.vocab_size = vocab_size
+        self.device = device
+        self.p_tags = p_tags
+        self.pad_idx=ignore_index
+
+        self.transformer_layer = nn.Transformer(hidden_dim, num_heads, num_layers_enc, num_layers_dec, dim_feedforward, dropout, batch_first=True)
+        
+        self.word_embedding = nn.Embedding(vocab_size, hidden_dim)
+        self.position_embedding = nn.Embedding(max_length, hidden_dim)
+        self.personality_layer = nn.Linear(p_tags, hidden_dim)
+
+        self.final_linear_layer = nn.Linear(hidden_dim, vocab_size)
+
+    def forward(self, input_text, output_text, input_personality):
+
+        batch_size,input_len = input_text.shape
+        _, output_len = output_text.shape
+        position_input = torch.arange(input_len).repeat(batch_size,1).to(device = self.device)
+        position_output = torch.arange(output_len).repeat(batch_size,1).to(device = self.device)
+
+
+        input_emb = self.position_embedding(position_input)
+        output_emb = self.position_embedding(position_output)
+
+        input_emb += self.word_embedding(input_text) # B,len, hidden
+        output_emb += self.word_embedding(output_text)
+
+        personality = F.softmax(input_personality, dim = -1) # B,hidden
+        personality = self.personality_layer(personality).unsqueeze(1) #B,1,hidden
+
+        input_emb += personality # B, len, hidden
+
+        input_padding_mask = input_text == self.pad_idx
+        output_padding_mask = output_text == self.pad_idx
+
+        output_mask = self.transformer_layer.generate_square_subsequent_mask(output_len,device=self.device)
+
+        out = self.transformer_layer(input_emb, output_emb, tgt_mask = output_mask,src_key_padding_mask=input_padding_mask , tgt_key_padding_mask = output_padding_mask)
+
+        out = self.final_linear_layer(out)
+
+        return out
--- utils/helpers.py
+++ utils/helpers.py
@@ -1,6 +1,7 @@
 import numpy as np
 import torch
-import transformers
+from torch.utils.data import DataLoader, Dataset
+import pandas as pd
 
 
 def find_backend():
@@ -8,3 +9,53 @@ def find_backend():
     device = 'mps' if torch.backends.mps.is_available() else ('cuda' if torch.cuda.is_available() else 'cpu')
     print("Currently using: ", device)
     return device
+
+def tokenize_column(column, tokenizer, max_length=None):
+    if max_length!=None:    
+        return column.apply(lambda x:tokenizer.encode(x[:max_length], max_length = max_length))
+    else:
+        return column.apply(lambda x:tokenizer.tokenize(x))
+    
+def encode_text(df, tokenizer, max_length):
+    df["tokenized"] = tokenize_column(df["text"], tokenizer)
+    df["encoded"] = tokenize_column(df["tokenized"], tokenizer, max_length=max_length)
+    return df
+
+class PersonalityTextDataset(Dataset):
+    def __init__(self, df, tokenizer, max_length, context_window=None):
+        self.df = df
+        self.tokenizer = tokenizer
+        self.max_lenth = max_length
+
+        # take full sentence if the window is not specified
+        self.context_window = context_window if context_window != None else max_length
+        self.processed_data = []
+        self.process_text()
+
+    def __len__(self):
+        return len(self.processed_data)
+    
+    def __getitem__(self, index):
+        return self.processed_data[index]
+    
+    def process_text(self, text, personality):
+
+        if not isinstance(personality, torch.Tensor):
+            personality = torch.tensor(personality)
+
+        tokens = self.tokenizer.encode(text, add_special_tokens = True,
+                                       max_length = self.max_lenth,
+                                    )
+        seq_len = tokens.shape[0]
+
+        if 1 < seq_len < self.context_window + 1:
+            
+
+
+    
+        
+
+        
+    
+
+    
